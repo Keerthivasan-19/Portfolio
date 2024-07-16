e296ffacfa34935119a7e136cf9eb8f68e433456
Keerthivasan <keerthivasandec1914@gmail.com>
2024-07-16 20:07:30 +0530

    Normalize line endings

---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+
+export default function Header() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <header className="flex justify-between px-5 py-7 bg-header">
+      <a className="font-bold" href="#">
+        Vasan Code
+      </a>
+      <nav className="hidden md:block">
+        <ul className="flex text-white">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      {toggleMenu && (
+        <nav className="block md:hidden ">
+          <ul className="flex flex-col text-white nav-mobile">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      )}
+      <button
+        onClick={() => setToggleMenu(!toggleMenu)}
+        className="block md:hidden"
+      >
+        <Bars3Icon className="h-6 text-white" />
+      </button>
+    </header>
+  );
+}
